a697da4b55a7c0164548b5069f8e78d6845aa7c6
Seth Condie <seth.condie@lovelandinnovations.com>
2023-09-28 11:08:09 -0600

    Initial commit with boiler plate Spring Boot code

---
+++ src/main/java/com/sethhaskellcondie/thegamepensiveapi/TheGamePensiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.sethhaskellcondie.thegamepensiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGamePensiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGamePensiveApiApplication.class, args);
+	}
+
+}
