03836539bc98c744c4fe1a641a9aadc5398f441c
Seth Condie <seth.condie@lovelandinnovations.com>
2024-01-19 15:59:53 -0700

    Included the rest of the Toy entity classes.

--- src/main/java/com/sethhaskellcondie/thegamepensiveapi/domain/system/SystemGatewayImpl.java
+++ src/main/java/com/sethhaskellcondie/thegamepensiveapi/domain/system/SystemGatewayImpl.java
@@ -1,17 +1,13 @@
 package com.sethhaskellcondie.thegamepensiveapi.domain.system;
 
-import java.util.List;
-
 import org.springframework.stereotype.Component;
 
 import com.sethhaskellcondie.thegamepensiveapi.domain.EntityGatewayImpl;
 import com.sethhaskellcondie.thegamepensiveapi.domain.EntityService;
 
 @Component
-public class SystemGatewayImpl extends EntityGatewayImpl<System, SystemRequestDto, SystemResponseDto>
-{
-	public SystemGatewayImpl(EntityService<System, SystemRequestDto, SystemResponseDto> service)
-	{
+public class SystemGatewayImpl extends EntityGatewayImpl<System, SystemRequestDto, SystemResponseDto> {
+	public SystemGatewayImpl(EntityService<System, SystemRequestDto, SystemResponseDto> service) {
 		super(service);
 	}
 }
