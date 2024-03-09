41ef5604e1cad992f29c98b1e1eccdb7620e2655
Seth Condie <seth.condie@lovelandinnovations.com>
2024-03-08 23:51:38 -0700

    Updated the entity tests.

--- src/test/java/com/sethhaskellcondie/thegamepensiveapi/domain/EntityTests.java
+++ src/test/java/com/sethhaskellcondie/thegamepensiveapi/domain/EntityTests.java
@@ -9,38 +9,38 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class EntityTests {
     @Test
     public void equals_PersistedAndNotPersisted_false() {
-        Entity persistedEntity = generatePersistedEntity(1);
-        Entity entity = generateEntity();
+        Entity<?, ?> persistedEntity = generatePersistedEntity(1);
+        Entity<?, ?> entity = generateEntity();
         assertNotEquals(entity, persistedEntity);
     }
 
     @Test
     public void equals_DifferentIds_False() {
-        Entity entity1 = generatePersistedEntity(1);
-        Entity entity2 = generatePersistedEntity(2);
+        Entity<?, ?> entity1 = generatePersistedEntity(1);
+        Entity<?, ?> entity2 = generatePersistedEntity(2);
         assertNotEquals(entity1, entity2);
     }
 
     @Test
     public void equals_EntityAndNull_False() {
-        Entity entity1 = generatePersistedEntity(1);
+        Entity<?, ?> entity1 = generatePersistedEntity(1);
         Integer notAnEntity = 1;
         assertNotEquals(entity1, notAnEntity);
     }
 
     @Test
     public void equals_SameIds_True() {
-        Entity entity1 = generatePersistedEntity(1);
-        Entity entity1Again = generatePersistedEntity(1);
+        Entity<?, ?> entity1 = generatePersistedEntity(1);
+        Entity<?, ?> entity1Again = generatePersistedEntity(1);
         assertEquals(entity1, entity1Again);
     }
 
-    //Since Entity are abstract we will use System Objects for the tests
-    private Entity generatePersistedEntity(Integer id) {
+    //Since Entities are abstract we will use System Objects for the tests
+    private Entity<?, ?> generatePersistedEntity(Integer id) {
         return new System(id, "test", 1, false);
     }
 
-    private Entity generateEntity() {
+    private Entity<?, ?> generateEntity() {
         return new System();
     }
 }
