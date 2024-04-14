1f36e63d3d20eba12071edc6f9a9132db5b239c2
Seth Condie <seth.condie@lovelandinnovations.com>
2024-04-13 22:43:25 -0600

    Included the MockMvc tests for Toys and Systems back in the project.

---
+++ src/test/java/com/sethhaskellcondie/thegamepensiveapi/domain/system/SystemTestRestTemplateTests.java
@@ -0,0 +1,297 @@
+package com.sethhaskellcondie.thegamepensiveapi.domain.system;
+
+import com.sethhaskellcondie.thegamepensiveapi.domain.filter.Filter;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * This is another class where I experiment with a new kind of integration test. This time using a SpringBootTest
+ * to run the application context on a real server through a random port. This will allow me to use a TestRestTemplate
+ * to run RestApi calls on the given server. This will run slower than the MockMvc but will run closer to a production
+ * environment. There is another iteration on the TestRestTemplate that will run async called a WebTestClient.
+ * A couple of big features for the TestRestTemplate is that it can be provided with auth credentials, and different
+ * options that can be used to test different parts of the system when running as a server.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test-container")
+public class SystemTestRestTemplateTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void postSystem_ValidPayload_SystemCreatedAndReturned() {
+        final String expectedName = "NES";
+        final int expectedGeneration = 3;
+        final boolean expectedHandheld = false;
+
+        final ResponseEntity<Map> response = this.postCustomSystem(expectedName, expectedGeneration, expectedHandheld);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        validateSystemResponseBody(response, expectedName, expectedGeneration, expectedHandheld);
+    }
+
+    @Test
+    void postSystem_FailedValidation_ReturnArrayOfErrors() {
+        final SystemRequestDto requestDto = new SystemRequestDto("", -1, null);
+        final HttpEntity<SystemRequestDto> request = new HttpEntity<>(requestDto);
+
+        final ResponseEntity<Map> response = restTemplate.postForEntity("/systems", request, Map.class);
+        final Map<String, Object> data = (Map<String, Object>) response.getBody().get("data");
+        final List<String> errors = (List<String>) response.getBody().get("errors");
+
+        assertAll(
+                "Error response wasn't returned properly on bad POST system request. (failed input validation)",
+                () -> assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode()),
+                () -> assertNull(data),
+                () -> assertEquals(3, errors.size())
+        );
+    }
+
+    @Test
+    void postSystem_SystemNameDuplicate_ReturnBadRequest() {
+        final String duplicateName = "Game Boy";
+        final int generation = 3;
+        final boolean handheld = true;
+
+        this.postCustomSystem(duplicateName, generation, handheld);
+
+        final SystemRequestDto requestDto = new SystemRequestDto(duplicateName, generation, handheld);
+        final HttpEntity<SystemRequestDto> request = new HttpEntity<>(requestDto);
+
+        final ResponseEntity<Map> response = restTemplate.postForEntity("/systems", request, Map.class);
+        final Map<String, Object> data = (Map<String, Object>) response.getBody().get("data");
+        final List<String> errors = (List<String>) response.getBody().get("errors");
+
+        assertAll(
+                "Error response wasn't returned properly on bad POST system request. (duplicate name)",
+                () -> assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode()),
+                () -> assertNull(data),
+                () -> assertEquals(1, errors.size())
+        );
+    }
+
+    @Test
+    void getOneSystem_SystemExists_SystemSerializedCorrectly() {
+        final String name = "Genesis";
+        final int generation = 4;
+        final boolean handheld = true;
+        final ResponseEntity<Map> postResponse = this.postCustomSystem(name, generation, handheld);
+        final SystemResponseDto expectedDto = resultToResponseDto(postResponse);
+
+        final ResponseEntity<Map> response = restTemplate.getForEntity("/systems/" + expectedDto.id(), Map.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        validateSystemResponseBody(response, name, generation, handheld);
+    }
+
+    @Test
+    void getOneSystem_SystemMissing_NotFoundReturned() {
+        final ResponseEntity<Map> response = restTemplate.getForEntity("/systems/-1", Map.class);
+        final Map<String, Object> data = (Map<String, Object>) response.getBody().get("data");
+        final List<String> errors = (List<String>) response.getBody().get("errors");
+
+        assertAll(
+                "Error response wasn't returned properly on bad POST system request. (not found)",
+                () -> assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode()),
+                () -> assertNull(data),
+                () -> assertEquals(1, errors.size())
+        );
+    }
+
+    //TODO return to this after get with filters has been implemented (it works but not in sequence with the other tests)
+    void getAllSystems_TwoSystemPresent_TwoSystemsReturnedInArray() {
+        final String name1 = "Super Nintendo";
+        final int generation1 = 4;
+        final boolean handheld1 = false;
+        final ResponseEntity<Map> result1 = this.postCustomSystem(name1, generation1, handheld1);
+        final SystemResponseDto responseDto1 = resultToResponseDto(result1);
+
+        final String name2 = "Sony Playstation";
+        final int generation2 = 4;
+        final boolean handheld2 = false;
+        final ResponseEntity<Map> result2 = this.postCustomSystem(name2, generation2, handheld2);
+        final SystemResponseDto responseDto2 = resultToResponseDto(result2);
+
+        final HttpEntity<Filter> request = new HttpEntity<>(new Filter("system", "field", "operator", "operand"));
+
+        final ResponseEntity<Map> response = restTemplate.postForEntity("/systems/search", request, Map.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        validateSystemResponseBody(response, List.of(responseDto1, responseDto2));
+    }
+
+    //TODO return to this after get with filters has been implemented (it works but not in sequence with the other tests)
+    void getAllSystems_NoSystemsPresent_EmptyArrayReturned() {
+        final HttpEntity<Filter> request = new HttpEntity<>(new Filter("system", "field", "operator", "operand"));
+
+        final ResponseEntity<Map> response = restTemplate.postForEntity("/systems/search", request, Map.class);
+        final List<Map<String, Object>> returnedSystems = (List<Map<String, Object>>) response.getBody().get("data");
+        final List<String> errors = (List<String>) response.getBody().get("errors");
+
+        assertAll(
+                "Response Body Incorrect on Empty Search Result",
+                () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
+                () -> assertEquals(0, returnedSystems.size()),
+                () -> assertNull(errors)
+        );
+    }
+
+    @Test
+    void updateExistingSystem_ValidUpdate_ReturnOk() {
+        final ResponseEntity<Map> existingResult = this.postSystem();
+        final SystemResponseDto responseDto = resultToResponseDto(existingResult);
+
+        final String newName = "Playstation 2";
+        final int newGeneration = 6;
+        final boolean newHandheld = false;
+        final SystemResponseDto expectedDto = new SystemResponseDto(responseDto.type(), responseDto.id(), newName, newGeneration, newHandheld);
+
+        final SystemRequestDto requestDto = new SystemRequestDto(newName, newGeneration, newHandheld);
+        final HttpEntity<SystemRequestDto> request = new HttpEntity<>(requestDto);
+
+        final ResponseEntity<Map> response = restTemplate.exchange("/systems/" + responseDto.id(), HttpMethod.PUT, request, Map.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        validateSystemResponseBody(response, expectedDto);
+    }
+
+    @Test
+    void updateExistingSystem_InvalidId_ReturnNotFound() {
+        final SystemRequestDto requestDto = new SystemRequestDto("ValidButMissing", 3, false);
+        final HttpEntity<SystemRequestDto> request = new HttpEntity<>(requestDto);
+
+        final ResponseEntity<Map> response = restTemplate.exchange("/systems/-1", HttpMethod.PUT, request, Map.class);
+        final Map<String, Object> data = (Map<String, Object>) response.getBody().get("data");
+        final List<String> errors = (List<String>) response.getBody().get("errors");
+
+        assertAll(
+                "Error response wasn't returned properly on bad PUT system request. (not found)",
+                () -> assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode()),
+                () -> assertNull(data),
+                () -> assertEquals(1, errors.size())
+        );
+    }
+
+    @Test
+    void deleteExistingSystem_SystemExists_ReturnNoContent() {
+        final ResponseEntity<Map> existingResult = this.postSystem();
+        final SystemResponseDto responseDto = resultToResponseDto(existingResult);
+
+        final ResponseEntity<Map> response = restTemplate.exchange("/systems/" + responseDto.id(), HttpMethod.DELETE, null, Map.class);
+
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        //Limitation: I may be doing this wrong, but I haven't been able to get a DELETE call to return a response body.
+        //So I can't check so make sure that the errors are empty and the data is ALSO empty.
+    }
+
+    @Test
+    void deleteExistingSystem_InvalidId_ReturnNotFound() {
+        final ResponseEntity<Map> response = restTemplate.exchange("/systems/-1", HttpMethod.DELETE, null, Map.class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        //Limitation: I may be doing this wrong, but I haven't been able to get a DELETE call to return a response body.
+        //So I can't check so make sure that the errors are empty and the data is ALSO empty.
+    }
+
+    private SystemResponseDto resultToResponseDto(ResponseEntity<Map> response) {
+        final Map<String, Object> data = (Map<String, Object>) response.getBody().get("data");
+        return new SystemResponseDto(
+                (String) data.get("type"),
+                (int) data.get("id"),
+                (String) data.get("name"),
+                (int) data.get("generation"),
+                (boolean) data.get("handheld")
+        );
+    }
+
+    private void validateSystemResponseBody(ResponseEntity<Map> response, String expectedName, int expectedGeneration, boolean expectedHandheld) {
+        final List<String> errors = (List<String>) response.getBody().get("errors");
+        //the SystemResponseDto is returned as a Map
+        final Map<String, Object> data = (Map<String, Object>) response.getBody().get("data");
+
+        assertAll(
+                "The SystemResponseBody was formatted incorrectly",
+                () -> assertEquals("system", data.get("type")),
+                () -> assertNotNull(data.get("id")),
+                () -> assertEquals(expectedName, data.get("name")),
+                () -> assertEquals(expectedGeneration, data.get("generation")),
+                () -> assertEquals(expectedHandheld, data.get("handheld")),
+                () -> assertNull(errors)
+        );
+    }
+
+    private void validateSystemResponseBody(ResponseEntity<Map> response, SystemResponseDto responseDto) {
+        final List<String> errors = (List<String>) response.getBody().get("errors");
+        final Map<String, Object> data = (Map<String, Object>) response.getBody().get("data");
+
+        assertAll(
+                "The SystemResponseBody was formatted incorrectly",
+                () -> assertEquals(responseDto.type(), data.get("type")),
+                () -> assertEquals(responseDto.id(), (int) data.get("id")),
+                () -> assertEquals(responseDto.name(), data.get("name")),
+                () -> assertEquals(responseDto.generation(), data.get("generation")),
+                () -> assertEquals(responseDto.handheld(), data.get("handheld")),
+                () -> assertNull(errors)
+        );
+    }
+
+    private void validateSystemResponseBody(ResponseEntity<Map> response, List<SystemResponseDto> expectedSystems) {
+        final List<Map<String, Object>> returnedSystems = (List<Map<String, Object>>) response.getBody().get("data");
+        final List<String> errors = (List<String>) response.getBody().get("errors");
+        assertNull(errors);
+
+        //testing order as well as each member being deserialized correctly
+        for (int i = 0; i < returnedSystems.size(); i++) {
+            SystemResponseDto expectedSystem = expectedSystems.get(i);
+            Map<String, Object> returnedSystem = returnedSystems.get(i);
+            assertAll(
+                    "The response body is not formatted correctly",
+                    () -> assertEquals("system", returnedSystem.get("type")),
+                    () -> assertEquals(expectedSystem.id(), (int) returnedSystem.get("id")),
+                    () -> assertEquals(expectedSystem.name(), returnedSystem.get("name")),
+                    () -> assertEquals(expectedSystem.generation(), (int) returnedSystem.get("generation")),
+                    () -> assertEquals(expectedSystem.handheld(), returnedSystem.get("handheld"))
+            );
+        }
+    }
+
+    // --------- Transplants from the TestFactory ---------
+    private String randomString(int length) {
+        return RandomStringUtils.random(length, true, true);
+    }
+
+    private ResponseEntity<Map> postSystem() {
+        final String name = "TestSystem-" + randomString(8);
+        final int generation = 1;
+        final boolean handheld = false;
+
+        return postCustomSystem(name, generation, handheld);
+    }
+
+    private ResponseEntity<Map> postCustomSystem(String name, int generation, boolean handheld) {
+        final SystemRequestDto requestDto = new SystemRequestDto(name, generation, handheld);
+        final System system = new System().updateFromRequestDto(requestDto);
+        final HttpEntity<System> request = new HttpEntity<>(system);
+
+        final ResponseEntity<Map> response = restTemplate.postForEntity("/systems", request, Map.class);
+        assertEquals(response.getStatusCode(), HttpStatus.CREATED);
+        return response;
+    }
+}
---
+++ src/test/java/com/sethhaskellcondie/thegamepensiveapi/domain/toy/ToyWebTestClientTests.java
@@ -0,0 +1,233 @@
+package com.sethhaskellcondie.thegamepensiveapi.domain.toy;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
+import org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * This is another class where I experiment with a new kind of integration test. This time using a WebTestClient
+ * this runs the application context as real server through a random port. The WebTestClient can be run with or without
+ * a server, it can be run similar to a MockMvc, and it's much more like that than running the TestRestTemplate.
+ * The big advantage of the WebTestClient over the TestRestTemplate is that the TestRestTemplate runs the requests
+ * synchronously while the WebTestClient will run the test asynchronously, so they will run faster overall.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test-container")
+public class ToyWebTestClientTests {
+
+    @Autowired
+    private WebTestClient client;
+
+    @Test
+    void postToy_ValidPayload_ToyCreatedAndReturned() {
+        final String expectedName = "Sora";
+        final String expectedSet = "Disney Infinity";
+
+        final ResponseSpec response = this.postCustomToy(expectedName, expectedSet);
+
+        response.expectStatus().isCreated();
+        validateToyResponseBody(response, expectedName, expectedSet);
+    }
+
+    @Test
+    void postNewToy_NameBlank_ReturnBadRequest() {
+        final String formattedJson = formatToyPayload("", "");
+
+        ResponseSpec response = client.post().uri("/toys").contentType(MediaType.APPLICATION_JSON).bodyValue(formattedJson).exchange();
+
+        response.expectStatus().isBadRequest();
+        response.expectHeader().contentType(MediaType.APPLICATION_JSON);
+        response.expectBody()
+                .jsonPath("$.data").isEmpty()
+                .jsonPath("$.errors").isNotEmpty();
+    }
+
+    @Test
+    void getOneToy_ToyExists_ToySerializedCorrectly() throws JsonProcessingException {
+        final String name = "Mario";
+        final String set = "Amiibo";
+        final ResponseSpec postResult = this.postCustomToy(name, set);
+        final ToyResponseDto expectedDto = resultToResponseDto(postResult);
+
+        ResponseSpec response = client.get().uri("/toys/" + expectedDto.id()).exchange();
+
+        response.expectStatus().isOk();
+        response.expectHeader().contentType(MediaType.APPLICATION_JSON);
+        validateToyResponseBody(response, expectedDto);
+    }
+
+    @Test
+    void getOneToy_ToyMissing_NotFoundReturned() {
+        ResponseSpec response = client.get().uri("/toys/-1").exchange();
+
+        response.expectStatus().isNotFound();
+        response.expectHeader().contentType(MediaType.APPLICATION_JSON);
+        response.expectBody()
+                .jsonPath("$.data").isEmpty()
+                .jsonPath("$.errors").isNotEmpty();
+    }
+
+    //TODO return to this after get with filters has been implemented (it works but not in sequence with the other tests)
+    void getAllToys_TwoToysPresent_TwoToysReturnedInArray() throws JsonProcessingException {
+        final String name1 = "MegaMan";
+        final String set1 = "Amiibo";
+        final ResponseSpec result1 = this.postCustomToy(name1, set1);
+        final ToyResponseDto toyDto1 = resultToResponseDto(result1);
+
+        final String name2 = "Goofy";
+        final String set2 = "Disney Infinity";
+        final ResponseSpec result2 = this.postCustomToy(name2, set2);
+        final ToyResponseDto toyDto2 = resultToResponseDto(result2);
+
+        final ResponseSpec response = client.post().uri("/toys/search").contentType(MediaType.APPLICATION_JSON).bodyValue("").exchange();
+
+        validateToyResponseBody(response, List.of(toyDto1, toyDto2));
+        response.expectStatus().isOk();
+        response.expectHeader().contentType(MediaType.APPLICATION_JSON);
+    }
+
+    //TODO return to this after get with filters has been implemented (it works but not in sequence with the other tests)
+    void getAllToys_NoToysPresent_EmptyArrayReturned() {
+        final ResponseSpec response = client.post().uri("/toys/search").contentType(MediaType.APPLICATION_JSON).bodyValue("").exchange();
+
+        response.expectStatus().isOk();
+        response.expectHeader().contentType(MediaType.APPLICATION_JSON);
+        response.expectBody()
+                .jsonPath("$.data").isEqualTo(new ArrayList<>())
+                .jsonPath("$.errors").isEmpty();
+    }
+
+    @Test
+    void putExistingToy_ValidUpdate_ReturnOk() throws JsonProcessingException {
+        final String name = "Donald Duck";
+        final String set = "Disney Infinity";
+        final ResponseSpec postResult = this.postCustomToy(name, set);
+        final ToyResponseDto expectedDto = resultToResponseDto(postResult);
+
+        final String newName = "Pit";
+        final String newSet = "Amiibo";
+
+        final String jsonContent = this.formatToyPayload(newName, newSet);
+        final ResponseSpec response = client.put().uri("/toys/" + expectedDto.id()).contentType(MediaType.APPLICATION_JSON).bodyValue(jsonContent).exchange();
+
+        response.expectStatus().isOk();
+        validateToyResponseBody(response, newName, newSet);
+    }
+
+    @Test
+    void updateExistingToy_InvalidId_ReturnNotFound() {
+
+        final String jsonContent = this.formatToyPayload("invalidId", "");
+        final ResponseSpec response = client.put().uri("/toys/-1").contentType(MediaType.APPLICATION_JSON).bodyValue(jsonContent).exchange();
+
+        response.expectStatus().isNotFound();
+        response.expectBody()
+                        .jsonPath("$.data").isEmpty()
+                        .jsonPath("$.errors").isNotEmpty();
+    }
+
+    @Test
+    void deleteExistingToy_ToyExists_ReturnNoContent() throws JsonProcessingException {
+        final ResponseSpec postResult = this.postToy();
+        final ToyResponseDto expectedDto = resultToResponseDto(postResult);
+
+        final ResponseSpec response = client.delete().uri("/toys/" + expectedDto.id()).exchange();
+
+        response.expectStatus().isNoContent();
+        //-Limitation- I can't get the body to return properly, it is always returned as null...
+    }
+
+    @Test
+    void deleteExistingToy_InvalidId_ReturnNotFound() {
+        final ResponseSpec response = client.delete().uri("/toys/-1").exchange();
+
+        response.expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("$.data").isEmpty()
+                .jsonPath("$.errors").isNotEmpty();
+    }
+
+    private void validateToyResponseBody(ResponseSpec result, String expectedName, String expectedSet) {
+        result.expectBody()
+                .jsonPath("$.data.type").isEqualTo("toy")
+                .jsonPath("$.data.type").isNotEmpty()
+                .jsonPath("$.data.name").isEqualTo(expectedName)
+                .jsonPath("$.data.set").isEqualTo(expectedSet);
+    }
+
+    private ToyResponseDto resultToResponseDto(ResponseSpec result) throws JsonProcessingException {
+        final String responseString = result.expectBody(String.class).returnResult().getResponseBody();
+        final Map<String, ToyResponseDto> body = new ObjectMapper().readValue(responseString, new TypeReference<>() { });
+        return body.get("data");
+    }
+
+    private void validateToyResponseBody(ResponseSpec result, ToyResponseDto expectedResponse) {
+        result.expectBody()
+                .jsonPath("$.data.type").isEqualTo("toy")
+                .jsonPath("$.data.id").isEqualTo(expectedResponse.id())
+                .jsonPath("$.data.name").isEqualTo(expectedResponse.name())
+                .jsonPath("$.data.set").isEqualTo(expectedResponse.set());
+    }
+
+    private void validateToyResponseBody(ResponseSpec result, List<ToyResponseDto> expectedToys) throws JsonProcessingException {
+        final String responseString = result.expectBody(String.class).returnResult().getResponseBody();
+        final Map<String, List<ToyResponseDto>> body = new ObjectMapper().readValue(responseString, new TypeReference<>() { });
+        final List<ToyResponseDto> returnedToys = body.get("data");
+        //test the order, and the deserialization
+        for (int i = 0; i < returnedToys.size(); i++) {
+            ToyResponseDto expectedToy = expectedToys.get(i);
+            ToyResponseDto returnedToy = returnedToys.get(i);
+            assertAll(
+                    "The response body for Toys is not formatted correctly",
+                    () -> assertEquals("toy", returnedToy.type()),
+                    () -> assertEquals(expectedToy.id(), returnedToy.id()),
+                    () -> assertEquals(expectedToy.name(), returnedToy.name()),
+                    () -> assertEquals(expectedToy.set(), returnedToy.set())
+            );
+        }
+    }
+
+    private String randomString(int length) {
+        return RandomStringUtils.random(length, true, true);
+    }
+
+    public ResponseSpec postToy() {
+        final String name = "TestToy-" + randomString(8);
+        final String set = "TestSet-" + randomString(8);
+        return postCustomToy(name, set);
+    }
+
+    public ResponseSpec postCustomToy(String name, String set) {
+        final String formattedJson = formatToyPayload(name, set);
+        ResponseSpec response = client.post().uri("/toys").contentType(MediaType.APPLICATION_JSON).bodyValue(formattedJson).exchange();
+
+        response.expectStatus().isCreated();
+        response.expectHeader().contentType(MediaType.APPLICATION_JSON);
+        return response;
+    }
+
+    public String formatToyPayload(String name, String set) {
+        final String json = """
+                {
+                	"name": "%s",
+                	"set": "%s"
+                }
+                """;
+        return String.format(json, name, set);
+    }
+}
