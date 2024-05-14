fa0e042ed6a7f8f93901d500b33d261a24ecfed4
Seth Condie <seth.condie@lovelandinnovations.com>
2024-05-14 17:52:02 -0600

    Cleaned up the EntityTests

--- src/test/java/com/sethhaskellcondie/thegamepensiveapi/domain/EntityTests.java
+++ src/test/java/com/sethhaskellcondie/thegamepensiveapi/domain/EntityTests.java
@@ -8,6 +8,7 @@ import java.time.Instant;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EntityTests {
     @Test
@@ -38,6 +39,18 @@ public class EntityTests {
         assertEquals(entity1, entity1Again);
     }
 
+    @Test
+    public void isPersisted_PersistedEntity_True() {
+        Entity<?, ?> entity = generatePersistedEntity(10);
+        assertTrue(entity.isPersisted());
+    }
+
+    @Test
+    public void isDeleted_DeletedEntity_True() {
+        Entity<?, ?> entity = generateDeletedEntity(100);
+        assertTrue(entity.isDeleted());
+    }
+
     //Since Entities are abstract we will use System Objects for the tests
     private Entity<?, ?> generatePersistedEntity(Integer id) {
         return new System(id, "test", 1, false, Timestamp.from(Instant.now()), Timestamp.from(Instant.now()), null);
@@ -45,5 +58,9 @@ public class EntityTests {
 
     private Entity<?, ?> generateEntity() {
         return new System();
+    }
+
+    private Entity<?, ?> generateDeletedEntity(Integer id) {
+        return new System(id, "deletedTest", 1, true, Timestamp.from(Instant.now()), Timestamp.from(Instant.now()), Timestamp.from(Instant.now()));
     }
 }
