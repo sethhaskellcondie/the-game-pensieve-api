a2d76b888254a94b0b3cd5213cb92a298a43dece
Seth Condie <seth.condie@lovelandinnovations.com>
2024-05-19 01:03:45 -0600

    Updated the custom fields migration.

--- src/main/resources/migrations/V1_3__AddCustomFields.sql
+++ src/main/resources/migrations/V1_3__AddCustomFields.sql
@@ -2,19 +2,18 @@ CREATE TABLE IF NOT EXISTS custom_fields (
     id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL,
     custom_field_type VARCHAR NOT NULL, -- This will be an enum enforced by the Repository
-    created_at timestamp with time zone NOT NULL,
-    updated_at timestamp with time zone NOT NULL,
-    deleted_at timestamp with time zone NULL
+    enabled BOOLEAN NOT NULL DEFAULT true
 );
 
 CREATE TABLE IF NOT EXISTS entity_to_custom_fields (
     entity_key VARCHAR NOT NULL,
-    custom_fields_id INTEGER REFERENCES custom_fields (id)
+    custom_fields_id INTEGER REFERENCES custom_fields (id),
+    enabled BOOLEAN NOT NULL DEFAULT true
 );
 
 CREATE TABLE IF NOT EXISTS custom_field_values (
     custom_fields_id INTEGER REFERENCES  custom_fields (id),
-    entity_id INTEGER NOT NULL, -- This will be a foreign key enforced by the Respository
+    entity_id INTEGER NOT NULL, -- This will be a foreign key enforced by the Repository
     entity_key VARCHAR NOT NULL,
     value VARCHAR
 );
