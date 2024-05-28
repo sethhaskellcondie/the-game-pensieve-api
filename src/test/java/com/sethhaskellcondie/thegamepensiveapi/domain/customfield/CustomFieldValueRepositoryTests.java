61c6d7e719a2f6993f667f33dbfea47141244534
Seth Condie <seth.condie@lovelandinnovations.com>
2024-05-28 00:34:32 -0600

    Removed unused import.

--- src/test/java/com/sethhaskellcondie/thegamepensiveapi/domain/customfield/CustomFieldValueRepositoryTests.java
+++ src/test/java/com/sethhaskellcondie/thegamepensiveapi/domain/customfield/CustomFieldValueRepositoryTests.java
@@ -15,7 +15,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
