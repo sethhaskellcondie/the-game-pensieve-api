74cae092f3eaae6c4f3b7aa500e98457ca3d81a7
sethcondie <sethhaskellcondie@gmail.com>
2024-07-05 15:42:19 -0600

    Implemented the gateways for the board game entities

--- src/main/java/com/sethhaskellcondie/thegamepensiveapi/domain/entity/boardgame/BoardGameGateway.java
+++ src/main/java/com/sethhaskellcondie/thegamepensiveapi/domain/entity/boardgame/BoardGameGateway.java
@@ -1,4 +1,14 @@
 package com.sethhaskellcondie.thegamepensiveapi.domain.entity.boardgame;
 
-public class BoardGameGateway {
+import com.sethhaskellcondie.thegamepensiveapi.domain.entity.EntityGateway;
+import com.sethhaskellcondie.thegamepensiveapi.domain.entity.EntityGatewayAbstract;
+import com.sethhaskellcondie.thegamepensiveapi.domain.entity.EntityService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BoardGameGateway extends EntityGatewayAbstract<BoardGame, BoardGameRequestDto, BoardGameResponseDto>
+        implements EntityGateway<BoardGame, BoardGameRequestDto, BoardGameResponseDto> {
+    public BoardGameGateway(EntityService<BoardGame, BoardGameRequestDto, BoardGameResponseDto> service) {
+        super(service);
+    }
 }
