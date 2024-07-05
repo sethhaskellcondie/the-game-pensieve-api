bdbd7df7e630363d094458903f7dcfe96f18603b
sethcondie <sethhaskellcondie@gmail.com>
2024-07-05 13:22:50 -0600

    Finished the boardgame repository.

--- src/main/java/com/sethhaskellcondie/thegamepensiveapi/domain/entity/boardgame/BoardGameService.java
+++ src/main/java/com/sethhaskellcondie/thegamepensiveapi/domain/entity/boardgame/BoardGameService.java
@@ -1,4 +1,36 @@
 package com.sethhaskellcondie.thegamepensiveapi.domain.entity.boardgame;
 
-public class BoardGameService {
+import com.sethhaskellcondie.thegamepensiveapi.domain.entity.EntityRepository;
+import com.sethhaskellcondie.thegamepensiveapi.domain.entity.EntityService;
+import com.sethhaskellcondie.thegamepensiveapi.domain.entity.EntityServiceAbstract;
+import com.sethhaskellcondie.thegamepensiveapi.domain.entity.boardgamebox.BoardGameBoxRepository;
+import com.sethhaskellcondie.thegamepensiveapi.domain.filter.FilterRequestDto;
+import com.sethhaskellcondie.thegamepensiveapi.domain.filter.FilterService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BoardGameService extends EntityServiceAbstract<BoardGame, BoardGameRequestDto, BoardGameResponseDto>
+        implements EntityService<BoardGame, BoardGameRequestDto, BoardGameResponseDto> {
+
+    private final BoardGameBoxRepository boardGameBoxRepository;
+
+    public BoardGameService(EntityRepository<BoardGame, BoardGameRequestDto, BoardGameResponseDto> repository, FilterService filterService, BoardGameBoxRepository boardGameBoxRepository) {
+        super(repository, filterService);
+        this.boardGameBoxRepository = boardGameBoxRepository;
+    }
+
+    @Override
+    public List<BoardGame> getWithFilters(List<FilterRequestDto> dtoFilters) {
+        final List<BoardGame> unPopulatedBoardGames = super.getWithFilters(dtoFilters);
+        final List<BoardGame> populatedBoardGames = new ArrayList<>();
+        for (BoardGame boardGame : unPopulatedBoardGames) {
+            //TODO after the boardGameBoxRepository is done use it to populate the list of board games boxes for each board game
+        }
+        return populatedBoardGames;
+    }
+
+
 }
